a99f0ba5dd0327611ef7798ab757216f40a45cee
genzo <genzo.pic@gmail.com>
2021-04-07 14:48:07 +0900

    ep.9

--- YoutubeApp/ViewControllers/VideoListViewController.swift
+++ YoutubeApp/ViewControllers/VideoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 //
 import Alamofire
 
-class VideoListController: UIViewController {
+class VideoListViewController: UIViewController {
     
     // ヘッダービュー
     @IBOutlet weak var headerView: UIView!
@@ -75,7 +75,7 @@ class VideoListController: UIViewController {
 //
 // MARK: - スクロール処理
 //
-extension VideoListController {
+extension VideoListViewController {
     // MARK: スクロールした時に呼ばれる
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         headerAnimation(scrollView: scrollView)
@@ -143,35 +143,33 @@ extension VideoListController {
 //
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 //
-extension VideoListController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension VideoListViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    // サイズ調整
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         let width = self.view.frame.width
         if indexPath.row == 2 {
+            // AttensionCollectionView
             return .init(width: width, height: 200)
         } else {
+            // VideoListCell
             return .init(width: width, height: width)
         }
-        
     }
-    
+    // セルの数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // AttentionCollectionViewの分をプラス１しておく
         return videoItems.count + 1
     }
-    
+    // セルの構築
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.row == 2 {
             let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: attentionCellId, for: indexPath) as! AttentionCell
-            
             cell.videoItems = self.videoItems
-            
             return cell
         } else {
             let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! VideoListCell
-            
             if self.videoItems.count == 0 { return cell }
-            
             if indexPath.row > 2 {
                 cell.videoItem = videoItems[indexPath.row - 1]
             } else {
@@ -179,6 +177,15 @@ extension VideoListController : UICollectionViewDelegate, UICollectionViewDataSo
             }
             return cell
         }
+    }
+    // タップで遷移
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("didSelectItemAt: ", indexPath.row)
+        let storyboard = UIStoryboard(name: "Video", bundle: nil)
+        let videoViewController = storyboard.instantiateViewController(identifier: "VideoViewController") as! VideoViewController
+        videoViewController.modalPresentationStyle = .fullScreen
+        present(videoViewController, animated: true, completion: nil)
+        
     }
     
 }
--- YoutubeApp/ViewControllers/VideoViewController.swift
+++ YoutubeApp/ViewControllers/VideoViewController.swift
@@ -11,8 +11,8 @@ class VideoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
 
